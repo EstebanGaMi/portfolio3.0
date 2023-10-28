5777a8990b253686a07d4e3c8e0553d430fc047e
EstebanGaMi <stevegaray1234@gmail.com>
2023-10-28 13:46:34 +0100

    metadata

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,10 @@
 import "./globals.css";
-
 import Cover from "./components/Cover";
+
+export const metadata = {
+  title: "Portfolio",
+  description: "This is my portfolio created with next.js",
+};
 
 export default function RootLayout({ children }) {
   return (
